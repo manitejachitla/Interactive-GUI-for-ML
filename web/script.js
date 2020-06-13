abe35c4d24b5b8eea9d61a18937d44699ba0342f
manitejachitla <30629503+manitejachitla@users.noreply.github.com>
2020-06-13 13:17:59 +0530

    Add files via upload

---
+++ web/script.js
@@ -0,0 +1,9 @@
+function handle_modal(i)
+{
+    document.getElementsByClassName('back')[i].style.display='block'
+
+}
+function close_modal(i)
+{
+     document.getElementsByClassName('back')[i].style.display='none'
+}
